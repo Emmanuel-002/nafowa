4d8aa482a057c9d2d862d1a6ce46bb9561d79afc
EMMANUEL ARAOYE <106038989+Emmanuel-002@users.noreply.github.com>
2024-07-28 13:28:46 +0100

    Update Video.js

--- src/home/Video.js
+++ src/home/Video.js
@@ -3,7 +3,7 @@ import video from '../video/video.mp4'
 
 const Video = () => {
     return(
-        <video src={video} loop={true} type="video/mp4" width="100%" autoPlay={true} muted={true}>
+        <video src={video} loop={true} width="100%" autoPlay={true} muted={true}>
         </video>
     )
 }
